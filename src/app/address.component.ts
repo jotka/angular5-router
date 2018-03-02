9ede6dad72f3c8b40f0e86a12c488e089c129072
Jaroslaw Krochmalski <XGT@bec.dk>
2018-03-02 11:07:39 +0100

    Initial commit

---
+++ src/app/address.component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+
+@Component({
+  templateUrl: 'address.component.html'
+})
+export class AddressComponent {
+}
---
+++ src/app/country/country.component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+
+@Component({
+  templateUrl: 'country.component.html'
+})
+export class CountryComponent {
+}
